65f8f66fcd96192ae6257693299d863a83dd40bb
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2024-10-25 10:02:54 -0500

    finalizacion de toda la api

--- Blog/settings.py
+++ Blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,9 +81,13 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "claseviernes",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
@@ -104,6 +109,17 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':datetime.timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME':datetime.timedelta(days=7),
+}
 
 
 # Internationalization
--- apps/user/models.py
+++ apps/user/models.py
@@ -4,6 +4,4 @@ from apps.rol.models import Rol
 
 # Create your models here.
 class User(AbstractUser):
-    cedula=models.CharField(max_length=30 unique=True)
-    edad=models.CharField(max_length=30 unique=True)
     rol=models.ForeignKey(Rol, on_delete=models.SET_NULL,null=True)
